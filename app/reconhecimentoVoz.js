d5e5ca1a45937a0e95ad71cbbe730f30517d5fba
CaciaRodrigues <caciarodriguues@gmail.com>
2023-07-21 16:04:38 -0300

    doc js reconhecimento de voz

---
+++ app/reconhecimentoVoz.js
@@ -0,0 +1,5 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br';
+recognition.start()
